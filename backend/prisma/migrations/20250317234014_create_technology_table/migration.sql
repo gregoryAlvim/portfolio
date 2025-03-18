1c1229eff39eb704591ff78df275b2d5ed08d125
Grégory Alvim <gregori.jagua@gmail.com>
2025-03-17 21:01:36 -0300

    feat: initial setup - 1 day

---
+++ backend/prisma/migrations/20250317234014_create_technology_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Technologies" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "emphasis" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Technologies_pkey" PRIMARY KEY ("id")
+);
