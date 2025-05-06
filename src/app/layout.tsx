70a601243dad0de02555a18b42127f38ddbff713
Grégory Alvim <gregori.jagua@gmail.com>
2025-05-05 21:17:16 -0300

    fix: some bugs from theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,41 +2,41 @@ import Header from "@/components/header";
 import PageTransition from "@/components/page-transition";
 import StairTransition from "@/components/stair-transition";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ThemeWrapper } from "@/components/theme-wrapper";
 import "@/css/globals.css";
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 
 const jetbrainsMon = JetBrains_Mono({
-	subsets: ["latin"],
-	variable: "--font-jetbrains-mono",
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
-	title: "Gregs",
-	description: "Seu SaaS de planejamento financeiro",
+  title: "Gregs",
+  description: "Seu SaaS de planejamento financeiro",
 };
 
 interface Props {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: Props) {
-	return (
-		<html lang="pt-BR" className={jetbrainsMon.variable}>
-			<body>
-				<ThemeProvider
-					defaultTheme="system"
-					enableSystem
-          >
-					<ThemeWrapper>
-						<Header />
-						<StairTransition />
-						<PageTransition>{children}</PageTransition>
-					</ThemeWrapper>
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html
+      lang="pt-BR"
+      suppressHydrationWarning
+      className={jetbrainsMon.variable}
+    >
+      <body>
+        <ThemeProvider>
+          <>
+            <Header />
+            <StairTransition />
+            <PageTransition>{children}</PageTransition>
+          </>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
